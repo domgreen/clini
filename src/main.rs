40dda71c7454f7e72156e38110c7bb90bd977a3d
Flakebi <flakebi@t-online.de>
2022-11-27 17:35:00 +0100

    Update dependencies and use clap derive
    Update clap to version 4 and use the derive feature to create arguments
    from a struct. This shortens the code to parse arguments.
    Also update the rust-ini crate.

--- src/main.rs
+++ src/main.rs
@@ -1,127 +1,95 @@
 extern crate exitcode;
 extern crate ini;
 
-use clap::{crate_version, App, Arg, ArgMatches};
+use clap::{Args, Parser, Subcommand};
 use ini::Ini;
 use std::io;
 
-// This is bad right?
-const INPUT: &'static str = "input";
-const SECTION: &'static str = "section";
-const KEY: &'static str = "key";
-const VALUE: &'static str = "value";
+#[derive(Debug, Parser)]
+#[command(author, version, about = "Basic ini file manipulation", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Args)]
+struct EntryArgs {
+    /// The `[section]` in a file
+    #[arg(long, short)]
+    section: String,
+    /// The `key=` in a file
+    #[arg(long, short)]
+    key: String,
+    /// Path to the input file
+    input: String,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Parse a file and extract a value
+    Get {
+        #[command(flatten)]
+        entry: EntryArgs,
+    },
+    /// Overwrite or add a value
+    Set {
+        #[command(flatten)]
+        entry: EntryArgs,
+        #[arg(long, short)]
+        value: String,
+    },
+    /// Delete a value from a file
+    Del {
+        #[command(flatten)]
+        entry: EntryArgs,
+    },
+}
 
 fn main() {
-    let matches = App::new("clini")
-        .about("Basic ini file manipulation.")
-        .author("mail@domgreen.com")
-        .version(crate_version!())
-        .subcommand(
-            App::new("get").args(&[
-                Arg::new(INPUT).takes_value(true).required(true),
-                Arg::new(SECTION)
-                    .short('s')
-                    .long(SECTION)
-                    .takes_value(true)
-                    .required(true),
-                Arg::new(KEY)
-                    .short('k')
-                    .long(KEY)
-                    .takes_value(true)
-                    .required(true),
-            ]),
-        )
-        .subcommand(
-            App::new("set").args(&[
-                Arg::new(INPUT).takes_value(true).required(true),
-                Arg::new(SECTION)
-                    .short('s')
-                    .long(SECTION)
-                    .takes_value(true)
-                    .required(true),
-                Arg::new(KEY)
-                    .short('k')
-                    .long(KEY)
-                    .takes_value(true)
-                    .required(true),
-                Arg::new(VALUE)
-                    .short('v')
-                    .long(VALUE)
-                    .takes_value(true)
-                    .required(true),
-            ]),
-        )
-        .subcommand(
-            App::new("del").args(&[
-                Arg::new(INPUT).takes_value(true).required(true),
-                Arg::new(SECTION)
-                    .short('s')
-                    .long(SECTION)
-                    .takes_value(true)
-                    .required(true),
-                Arg::new(KEY)
-                    .short('k')
-                    .long(KEY)
-                    .takes_value(true)
-                    .required(true),
-            ]),
-        )
-        .get_matches();
+    let matches = Cli::parse();
 
-    match matches.subcommand() {
-        Some(("get", get_matches)) => match get_val(get_matches) {
+    match &matches.command {
+        Commands::Get { entry } => match get_val(entry) {
             None => {
                 std::process::exit(exitcode::UNAVAILABLE);
             }
             Some(val) => println!("{}", val),
         },
-        Some(("set", set_matches)) => {
-            set_val(set_matches).unwrap_or_else(|error| {
-                println!("Error: {}", error);
-            });
-        }
-        Some(("del", del_matches)) => {
-            del_value(del_matches).unwrap_or_else(|error| {
-                println!("Error: {}", error);
-            });
-        }
-        None => println!("No subcommand was used"), // If no subcommand was used it'll match the tuple ("", None)
-        _ => unreachable!(), // If all subcommands are defined above, anything else is unreachabe!()
+        Commands::Set { entry, value } => set_val(entry, value).unwrap_or_else(|error| {
+            println!("Error: {}", error);
+        }),
+        Commands::Del { entry } => del_val(entry).unwrap_or_else(|error| {
+            println!("Error: {}", error);
+        }),
     }
 
     std::process::exit(exitcode::OK);
 }
 
-fn set_val(set_matches: &ArgMatches) -> Result<(), io::Error> {
-    let input: &str = set_matches.value_of(INPUT).unwrap();
-    let section: &str = set_matches.value_of(SECTION).unwrap();
-    let param: &str = set_matches.value_of(KEY).unwrap();
-    let val: &str = set_matches.value_of(VALUE).unwrap();
-
-    let mut conf: Ini = Ini::load_from_file(input).unwrap();
-    conf.with_section(Some(section)).set(param, val);
-    conf.write_to_file(input)
+impl EntryArgs {
+    fn load(&self) -> Ini {
+        Ini::load_from_file(&self.input).unwrap_or_else(|error| {
+            println!("Error: {}", error);
+            std::process::exit(exitcode::DATAERR);
+        })
+    }
 }
 
-fn get_val(get_matches: &ArgMatches) -> Option<String> {
-    let input: &str = get_matches.value_of(INPUT)?;
-    let section: &str = get_matches.value_of(SECTION)?;
-    let key: &str = get_matches.value_of(KEY)?;
+fn set_val(entry: &EntryArgs, value: &str) -> Result<(), io::Error> {
+    let mut conf: Ini = entry.load();
+    conf.with_section(Some(&entry.section))
+        .set(&entry.key, value);
+    conf.write_to_file(&entry.input)
+}
 
-    let conf: Ini = Ini::load_from_file(input.to_string()).unwrap_or_else(|error| {
-        println!("Error: {}", error);
-        std::process::exit(exitcode::DATAERR);
-    });
-    let value: &str = &conf.section(Some(section))?.get(key)?;
+fn get_val(entry: &EntryArgs) -> Option<String> {
+    let conf: Ini = entry.load();
+    let value: &str = &conf.section(Some(&entry.section))?.get(&entry.key)?;
     Some(value.to_string())
 }
 
-fn del_value(del_matches: &ArgMatches) -> Result<(), io::Error> {
-    let input: &str = del_matches.value_of(INPUT).unwrap();
-    let section: &str = del_matches.value_of(SECTION).unwrap();
-    let key: &str = del_matches.value_of(KEY).unwrap();
-
-    let mut conf: Ini = Ini::load_from_file(input).unwrap();
-    conf.with_section(Some(section)).delete(&key);
-    conf.write_to_file(input)
+fn del_val(entry: &EntryArgs) -> Result<(), io::Error> {
+    let mut conf: Ini = entry.load();
+    conf.with_section(Some(&entry.section)).delete(&entry.key);
+    conf.write_to_file(&entry.input)
 }
